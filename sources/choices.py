1019aca12034eea2e698659fa5a7ef75b0b4eab8
Greg Linch <glinch@mcclatchydc.com>
2017-07-23 21:15:03 -0400

    add ratings choices

--- sources/choices.py
+++ sources/choices.py
@@ -10,3 +10,13 @@ PREFIX_CHOICES = (
     ('Ms.', 'Ms.'),
     ('Mx.', 'Mx.')
 )
+
+RATING_CHOICES = (
+	('1', 'Within 1 hour'),
+	('3', 'Within 3 hours'),
+	('6', 'Within 6 hours'),
+	('12', 'Within 12 hours'),
+	('24', 'Within 24 hours'),
+	('Y', 'Greater than 24 hours'),
+	('N', 'No response')
+)
