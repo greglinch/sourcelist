5fd69fcf38482c9853a112ef14273f266e227975
Greg Linch <glinch@mcclatchydc.com>
2017-08-12 20:42:59 -0400

    update admin with new fields

--- sources/admin.py
+++ sources/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Topic, Person, Location, Organization
+from .models import Expertise, Person, Language, Organization # Location,
 
 
-class TopicAdmin(admin.ModelAdmin):
+class ExpertiseAdmin(admin.ModelAdmin):
     fields = ['name']
     list_display = ['name']
     # list_editable = ['']
@@ -12,12 +12,21 @@ class TopicAdmin(admin.ModelAdmin):
 
 
 class PersonAdmin(admin.ModelAdmin):
-    fields = ['role', 'prefix', 'first_name', 'last_name', 'title', 'organization', 'website', 'topic', 'location', 'email_address', 'phone_number', 'woman', 'underrepresented', 'notes', 'rating'] 
-    list_display = ['last_name', 'first_name', 'role', 'country', 'timezone', 'title', 'id_as_woman', 'id_as_underrepresented', 'rating' ] ## 'email_address', 'phone_number', 'website', 'first_last_name', 
+    # fieldsets = (
+    #     (None, {
+
+    #     }),
+    #     ('Location', {
+            # 'fields': ('timezone', 'city', 'state', 'country')
+    #     }),
+    #     (, {}),
+    # )
+    fields = ['role', 'prefix', 'first_name', 'middle_name', 'last_name', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'notes', 'rating', 'language', 'timezone', 'city', 'state', 'country'] # 'location', 'woman', 'underrepresented',
+    list_display = ['last_name', 'first_name', 'role', 'country', 'timezone', 'title', 'rating' ] ## 'email_address', 'phone_number', 'website', 'first_last_name', 'id_as_woman', 'id_as_underrepresented',
     # list_editable = ['']
-    list_filter = ['role', 'underrepresented', 'woman', 'timezone', 'city', 'state', 'country', 'rating'] ## , 'title'
-    search_fields = ['email_address', 'topic', 'first_name', 'last_name', 'location', 'notes', 'organization', 'title', 'underrepresented', 'website']
-    filter_horizontal = ['topic', 'organization']
+    list_filter = ['role', 'rating', 'timezone', 'city', 'state', 'country'] ## , 'title', 'underrepresented', 'woman'
+    search_fields = ['city', 'country', 'email_address', 'expertise', 'first_name', 'last_name', 'notes', 'organization', 'state', 'title', 'underrepresented', 'website'] # 'location', 
+    filter_horizontal = ['expertise', 'organization', 'language']
     readonly_fields = ['rating']
     # exclude  = ['']
 
@@ -34,17 +43,26 @@ class PersonAdmin(admin.ModelAdmin):
     #         # return qs.filter(newsroom=request.user.documentcloudcredentials.newsroom)
 
 
-class LocationAdmin(admin.ModelAdmin):
-    fields = ['city', 'state', 'country', 'timezone']
-    list_display = ['city', 'state', 'country', 'timezone']
+class LanguageAdmin(admin.ModelAdmin):
+    # fields = ['']
+    list_display = ['name']
     # list_editable = ['']
-    list_filter = ['state', 'country', 'timezone']
-    search_fields = ['city', 'state', 'country', 'timezone']
+    # list_filter = ['']
+    search_fields = ['']
     # exclude  = ['']
 
 
+# class LocationAdmin(admin.ModelAdmin):
+#     fields = ['city', 'state', 'country', 'timezone']
+#     list_display = ['city', 'state', 'country', 'timezone']
+#     # list_editable = ['']
+#     list_filter = ['state', 'country', 'timezone']
+#     search_fields = ['city', 'state', 'country', 'timezone']
+#     # exclude  = ['']
+
+
 class OrganizationAdmin(admin.ModelAdmin):
-    fields = ['name', 'location', 'website']
+    fields = ['name', 'website'] # 'location',
     list_display = ['name', 'website']
     # list_editable = ['']
     # list_filter = ['']
@@ -81,9 +99,10 @@ class OrganizationAdmin(admin.ModelAdmin):
 
 ## TEMPLATE
 # admin.site.register(Model, ModelAdmin)
-admin.site.register(Topic, TopicAdmin)
+admin.site.register(Expertise, ExpertiseAdmin)
 admin.site.register(Person, PersonAdmin)
-admin.site.register(Location, LocationAdmin)
+admin.site.register(Language, LanguageAdmin)
+# admin.site.register(Location, LocationAdmin)
 admin.site.register(Organization, OrganizationAdmin)
 # admin.site.register(Journalist, JournalistAdmin)
 # admin.site.register(Source, SourceAdmin)
