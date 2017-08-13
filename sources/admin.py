7c2ce3fdf406e47612081e778b717dce22458511
Greg Linch <glinch@mcclatchydc.com>
2017-08-13 18:28:40 -0400

    add save_model method to associate a Person with a User who a) created them and B) relates to them; create User if not there yet

--- sources/admin.py
+++ sources/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import Expertise, Person, Language, Organization # Location,
 
 
@@ -42,6 +44,40 @@ class PersonAdmin(admin.ModelAdmin):
     #         return qs.filter(user=request.user)
     #         # return qs.filter(newsroom=request.user.documentcloudcredentials.newsroom)
 
+    def save_model(self, request, obj, form, change):
+        ## based on the Person being created, create a new User if they don't exist
+        try:
+            user_existing = User.objects.get(email=obj.email_address)
+        except:
+            user_existing = False
+        if user_existing:
+            obj.related_user = user_existing
+        else:
+            username = '{}{}'.format(obj.first_name, obj.last_name).lower().replace('-','')
+            import random
+            choices = 'abcdefghijklmnopqrstuvwxyz0123456789'
+            middle_choices = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+            password = \
+                ''.join([random.SystemRandom().choice(choices) for i in range(1)]) + \
+                ''.join([random.SystemRandom().choice(middle_choices) for i in range(23)]) + \
+                ''.join([random.SystemRandom().choice(choices) for i in range(1)])
+            user_new = User.objects.create_user(username, password=password)
+            user_new.email = obj.email_address
+            user_new.first_name = obj.first_name
+            user_new.last_name = obj.last_name
+            user_new.save()
+            ## trigger mgmt cmd to notify user they've been created and by whom
+            # from django.core import management.call_command
+            # call_command()
+        # except:
+        #     message = 'Error adding user.'
+        #     messages.error(request, message)
+        ## associate the Person being created with the User who created them
+        current_user = request.user
+        obj.created_by = current_user
+        ## save
+        super(PersonAdmin, self).save_model(request, obj, form, change)
+
 
 class LanguageAdmin(admin.ModelAdmin):
     # fields = ['']
@@ -62,11 +98,11 @@ class LanguageAdmin(admin.ModelAdmin):
 
 
 class OrganizationAdmin(admin.ModelAdmin):
-    fields = ['name', 'website'] # 'location',
-    list_display = ['name', 'website']
+    fields = ['name'] # 'location',
+    list_display = ['name']
     # list_editable = ['']
     # list_filter = ['']
-    search_fields = ['name', 'website']
+    search_fields = ['name']
     # exclude  = ['']
 
 
