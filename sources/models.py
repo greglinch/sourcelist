52fa33d9acf7b0bd3b621041454f7305fea771b2
Greg Linch <glinch@mcclatchydc.com>
2017-08-12 20:28:10 -0400

    update models for current needed fields and setup

--- sources/models.py
+++ sources/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
-from sources.choices import PERSON_CHOICES, PREFIX_CHOICES, RATING_CHOICES
+from django.core.validators import MinValueValidator, MaxValueValidator
+from sources.choices import PERSON_CHOICES, PREFIX_CHOICES, RATING_CHOICES, COUNTRY_CHOICES
 
 
 class BasicInfo(models.Model):
@@ -13,82 +14,83 @@ class BasicInfo(models.Model):
         abstract = True
 
 
-class Topic(BasicInfo):
+class Expertise(BasicInfo):
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Expertise'
+
+    def __unicode__(self):
+        return '%s' % (self.name)
+
+
+class Language(BasicInfo):
     name = models.CharField(max_length=255, null=True, blank=True)
 
     def __unicode__(self):
-        return "%s" % (self.name)
-
-
-class Location(BasicInfo):
-    """ for Person and Organization """
-    city = models.CharField(max_length=255, null=True, blank=True)
-    country = models.CharField(max_length=255, null=True, blank=True)
-    state = models.CharField(max_length=255, null=True, blank=True)
-    timezone = models.CharField(max_length=255, null=True, blank=True, verbose_name="Time zone") ## lookup based on city/state/county combo?
-
-    def __unicode__(self):
-        return "%s, %s, %s" % (self.city, self.state, self.country)
+        return '%s' % (self.name)
 
 
 class Organization(BasicInfo):
     """ for Sources and Journalists """
     name = models.CharField(max_length=255, null=True, blank=True)
-    location = models.ForeignKey(Location, null=True, blank=True)
+    # location = models.ForeignKey(Location, null=True, blank=True)
     website = models.URLField(max_length=200, null=True, blank=True)
 
     def __unicode__(self):
-        return "%s" % (self.name)
+        return '%s' % (self.name)
 
 
 class Person(BasicInfo):
     """ class to be inherited by Sources and Journalists """
     approved = models.BooleanField(default=False)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=255, choices=COUNTRY_CHOICES, null=True)
     email_address = models.EmailField(max_length=254)
+    expertise = models.ManyToManyField(Expertise, blank=True)
     first_name = models.CharField(max_length=255, null=True, blank=False)
     last_name = models.CharField(max_length=255, null=True, blank=False)
-    location = models.ForeignKey(Location, null=True, blank=True)
+    middle_name = models.CharField(max_length=255, null=True, blank=True)
+    language = models.ManyToManyField(Language)
+    # location = models.ForeignKey(Location, null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
     organization = models.ManyToManyField(Organization, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     prefix = models.CharField(choices=PREFIX_CHOICES, max_length=5, null=True, blank=True)
-    rating = models.PositiveIntegerField(null=True, blank=True) ## or should it be choices field with 1-10?
-    title = models.CharField(max_length=255, null=True, blank=True)
+    rating = models.PositiveIntegerField(null=True, blank=True)
+    rating_avg = models.IntegerField(null=True, blank=True)
     role = models.CharField(choices=PERSON_CHOICES, max_length=255, null=True, blank=False)
-    topic = models.ManyToManyField(Topic, blank=True)
-    underrepresented = models.BooleanField(default=False, verbose_name="Do you identify as a member of an underrepresented group?")
-    website = models.CharField(max_length=255, null=True, blank=True)
-    woman = models.BooleanField(default=False, verbose_name="Do you identify as a woman?")
-    ## updated from Location fk
-    city = models.CharField(max_length=255, null=True, blank=True)
-    country = models.CharField(max_length=255, null=True, blank=True)
     state = models.CharField(max_length=255, null=True, blank=True)
-    timezone = models.CharField(max_length=255, null=True, blank=True, verbose_name="Time zone") ## lookup based on city/state/county combo?
+    title = models.CharField(max_length=255, null=True, blank=True)
+    timezone = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(-12),MaxValueValidator(12)], verbose_name="Time zone offset") ## lookup based on city/state/county combo?
+    # underrepresented = models.BooleanField(default=False, verbose_name="Do you identify as a member of an underrepresented group?")
+    website = models.URLField(max_length=255, null=True, blank=True)
+    # woman = models.BooleanField(default=False, verbose_name="Do you identify as a woman?")
 
     def first_last_name(self):
-        return '%s %s' % (self.first_name, self.last_name)
+        if self.middle_name:
+            return '{} {}'.format(self.first_name, self.middle_name, self.last_name)
+        else:
+            return '{} {}'.format(self.first_name, self.last_name)
     first_last_name.short_description = "Name"
         
-    def id_as_woman(self):
-        return self.woman
-    id_as_woman.short_description = "Woman?"
-    id_as_woman.boolean = True
+    # def id_as_woman(self):
+    #     return self.woman
+    # id_as_woman.short_description = "Woman?"
+    # id_as_woman.boolean = True
 
-    def id_as_underrepresented(self):
-        return self.underrepresented
-    id_as_underrepresented.short_description = "Underrepresented?"
-    id_as_underrepresented.boolean = True
+    # def id_as_underrepresented(self):
+    #     return self.underrepresented
+    # id_as_underrepresented.short_description = "Underrepresented?"
+    # id_as_underrepresented.boolean = True
 
     def save(self, *args, **kwargs):
-        self.city = self.location.city
-        self.state = self.location.state
-        self.country = self.location.country
-        self.timezone = self.location.timezone
-
+        ## avg of all ratings
+        # self.rating_avg = # Aggregate Avg of all ratings for this user
         return super(Person, self).save(*args, **kwargs) 
 
     def __unicode__(self):
-        return "%s %s %s" % (self.prefix, self.first_name, self.last_name)
+        return '{} {}'.format(self.first_name, self.last_name)
 
     class Meta:
         verbose_name_plural = "People"
@@ -96,7 +98,8 @@ class Person(BasicInfo):
 
 class Rating(BasicInfo):
     """ a Journalist can rate a Source each time """
-    rating = models.TextField(choices=RATING_CHOICES, null=True, blank=True, max_length=255)
+    # notes = models.TextField(null=True, blank=True, help_text='Optional')
+    rating = models.CharField(choices=RATING_CHOICES, null=True, blank=True, max_length=255)
     user = models.ForeignKey(User)
 
     def __unicode__(self):
@@ -109,6 +112,8 @@ class Rating(BasicInfo):
 # class Journalist(Person):
 #     """ people who use the Sources """
 
+#     class Meta:
+#         proxy = True
 
 #     def __unicode__(self):
 #         return "%s %s %s" % (self.prefix, self.first_name, self.last_name)
@@ -116,6 +121,8 @@ class Rating(BasicInfo):
 # class Source(Person):
 #     """ sources for Journalists """
 
+#     class Meta:
+#         proxy = True
 
 #     def __unicode__(self):
 #         return "%s %s %s" % (self.prefix, self.first_name, self.last_name)
