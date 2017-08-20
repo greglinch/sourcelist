93afc10afe7c53f8ff22efd6c962df4083ed98d1
Greg Linch <glinch@mcclatchydc.com>
2017-08-20 13:42:29 -0400

    fix urls for debug toolbar

--- sourcelist/urls.py
+++ sourcelist/urls.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from sources.views import index
-from sourcelist import settings
 
 
 urlpatterns = [
