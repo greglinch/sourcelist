03d15849a7a2ca7c89dcb39571b4cd450a2ec302
Greg Linch <glinch@mcclatchydc.com>
2017-08-20 13:44:26 -0400

    move set_related_user things to that mgmt cmd and remove, add logic to set added workflows

--- sources/admin.py
+++ sources/admin.py
@@ -23,7 +23,7 @@ class PersonAdmin(admin.ModelAdmin):
     #     }),
     #     (, {}),
     # )
-    fields = ['role', 'prefix', 'first_name', 'middle_name', 'last_name', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'notes', 'rating', 'language', 'timezone', 'city', 'state', 'country'] # 'location', 'woman', 'underrepresented',
+    # fields = ['role', 'prefix', 'first_name', 'middle_name', 'last_name', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'notes', 'rating', 'language', 'timezone', 'city', 'state', 'country'] # 'location', 'woman', 'underrepresented',
     list_display = ['last_name', 'first_name', 'role', 'country', 'timezone', 'title', 'rating' ] ## 'email_address', 'phone_number', 'website', 'first_last_name', 'id_as_woman', 'id_as_underrepresented',
     # list_editable = ['']
     list_filter = ['role', 'rating', 'timezone', 'city', 'state', 'country'] ## , 'title', 'underrepresented', 'woman'
@@ -45,36 +45,20 @@ class PersonAdmin(admin.ModelAdmin):
     #         # return qs.filter(newsroom=request.user.documentcloudcredentials.newsroom)
 
     def save_model(self, request, obj, form, change):
-        ## based on the Person being created, create a new User if they don't exist
-        try:
-            user_existing = User.objects.get(email=obj.email_address)
-        except:
-            user_existing = False
-        if user_existing:
-            obj.related_user = user_existing
-        else:
-            username = '{}{}'.format(obj.first_name, obj.last_name).lower().replace('-','')
-            import random
-            choices = 'abcdefghijklmnopqrstuvwxyz0123456789'
-            middle_choices = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-            password = \
-                ''.join([random.SystemRandom().choice(choices) for i in range(1)]) + \
-                ''.join([random.SystemRandom().choice(middle_choices) for i in range(23)]) + \
-                ''.join([random.SystemRandom().choice(choices) for i in range(1)])
-            user_new = User.objects.create_user(username, password=password)
-            user_new.email = obj.email_address
-            user_new.first_name = obj.first_name
-            user_new.last_name = obj.last_name
-            user_new.save()
-            ## trigger mgmt cmd to notify user they've been created and by whom
-            # from django.core import management.call_command
-            # call_command()
-        # except:
-        #     message = 'Error adding user.'
-        #     messages.error(request, message)
         ## associate the Person being created with the User who created them
         current_user = request.user
         obj.created_by = current_user
+
+        ## set the status
+        if not obj.created:
+            if current_user.is_superuser == True:
+                status = 'added_by_admin'
+            elif current_user.email == obj.email:
+                status = 'added_by_self'
+            else:
+                status = 'added_by_other'
+        obj.status = status
+
         ## save
         super(PersonAdmin, self).save_model(request, obj, form, change)
 
