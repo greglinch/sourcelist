96649e6d41edd7a53be0b7617721f371fbaeb186
Greg Linch <glinch@mcclatchydc.com>
2017-09-02 16:27:33 -0400

    update comments in views

--- sources/views.py
+++ sources/views.py
@@ -11,20 +11,26 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+# def confirm_person_by_person(request):
+#     ## trigger mgmt cmd or, ideally, just the related function
+#     ## or just put the code here!
+
+#     return render(request, 'confirmation.html', context)
+
 def submit(request):
-    # if this is a POST request we need to process the form data
+    ## if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
+        ## create a form instance and populate it with data from the request:
         form = SubmitForm(request.POST)
-        # check whether it's valid:
+        ## check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
+            ## process the data in form.cleaned_data if needed
             # form.cleaned_data
-            person = form.save(commit=False)
-            person.save()
+            # person = form.save(commit=False)
+            # person.save()
+            form.save()
             # redirect to a new URL:
             return HttpResponseRedirect('/thanks/')
-
     # if a GET (or any other method) we'll create a blank form
     else:
         form = SubmitForm()
