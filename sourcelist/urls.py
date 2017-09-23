bc2f6d2de296bff73fb2e7264b1129eef88853e8
Greg Linch <glinch@mcclatchydc.com>
2017-09-23 17:24:55 -0400

    update views to include about and contact pages, rename submit as join

--- sourcelist/urls.py
+++ sourcelist/urls.py
@@ -2,21 +2,26 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from sources.views import index, confirm, submit, thankyou
+from sources.views import about, confirm, contact, index, join, thankyou
 
 
 urlpatterns = [
+    ## patterns from sources app
 	url(r'^sources/', include('sources.urls')),
+    ## general
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', auth_views.LoginView.as_view()),
+    ## site pages
+    url(r'^$', index, name='index'),
+    url(r'^index$', index, name='index'),
+    url(r'^about$', about, name='about'),
+    url(r'^contact$', contact, name='contact'),
     ## submission form
     url(r'^confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         confirm, name='confirm'), ## UPDATE?!?!
-    url(r'^submit', submit, name='submit'),
+    url(r'^join', join, name='join'),
     url(r'^thank-you', thankyou, name='thank-you'),
     ## social auth
-    url(r'^$', index, name='index'),
-    url(r'^index$', index, name='index'),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view()),
     # url('', include('social.apps.django_app.urls', namespace='social')),
     url('', include('social_django.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')),
--- sources/views.py
+++ sources/views.py
@@ -1,11 +1,13 @@
+from django.core.mail import send_mail
 from django.core.management import call_command
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render # , redirect
 from django.template.context import RequestContext
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.html import format_html
 from .tokens import account_confirmation_token
-from sources.forms import SubmitForm
+from sources.forms import ContactForm, SubmitForm
 # from django.contrib.auth import login, authenticate
 # from django.contrib.sites.shortcuts import get_current_site
 # from django.template.loader import render_to_string
@@ -21,6 +23,15 @@ def index(request):
         'user': request.user
     }
     return render(request, 'index.html', context)
+
+def about(request):
+    """ about page """
+
+    context = {
+        'request': request,
+        'user': request.user
+    }
+    return render(request, 'about.html', context)
 
 def confirm(request, uidb64, token):
     """ trigger mgmt cmd or, ideally, just the related function or just put the code here! """
@@ -42,7 +53,41 @@ def confirm(request, uidb64, token):
     ## see which token the user matches
     return render(request, 'confirmation.html', context)
 
-def submit(request):
+def contact(request):
+    """ contact page """
+
+    if request.method == 'POST':
+        ## create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        ## check whether it's valid:
+        if form.is_valid():
+            ## extract the necessary value for sending emails
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            name = form.cleaned_data['name']
+            plain_message = ''
+            html_message = '<table> \
+                <tr><td>Name:</td><td>{}</td></tr> \
+                <tr><td>Email:</td><td>{}</td></td></tr> \
+                <tr><td>Message:</td><td>{}</td></td></tr> \
+                </table> \
+                '.format(name, email, message)
+            send_mail(
+                'DivSciSources message from {}'.format(name),
+                plain_message,
+                'greglinch@gmail.com',
+                ['greglinch@gmail.com'],
+                html_message=html_message,
+                )
+            # redirect to thank you page:
+            return HttpResponseRedirect('/thank-you/')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
+
+def join(request):
     """ submission of a new source """
 
     ## if this is a POST request we need to process the form data
@@ -65,7 +110,7 @@ def submit(request):
     else:
         form = SubmitForm()
 
-    return render(request, 'submit.html', {'form': form})
+    return render(request, 'join.html', {'form': form})
 
 def thankyou(request):
     """ thank you page after submission """
