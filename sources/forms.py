5772b4ba3dc96666f4d691fc741d0f5b319ecd26
Greg Linch <glinch@mcclatchydc.com>
2017-09-23 17:23:04 -0400

    add contact form to forms.py

--- sources/forms.py
+++ sources/forms.py
@@ -1,8 +1,16 @@
-from django.forms import ModelForm
+from django import forms
 from sources.models import Person
 
 
-class SubmitForm(ModelForm):
-	class Meta:
-		model = Person
-		fields = ['prefix', 'first_name', 'middle_name', 'last_name', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'notes', 'language', 'timezone', 'city', 'state', 'country', 'status']
+class ContactForm(forms.Form):
+    name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(
+        required=True,
+        widget=forms.Textarea
+    )
+
+class SubmitForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = ['prefix', 'first_name', 'middle_name', 'last_name', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'notes', 'language', 'timezone', 'city', 'state', 'country', 'status']
