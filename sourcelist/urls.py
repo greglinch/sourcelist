daf1c2d89fc2ceaccb82a6628a71569c2b9bba61
Greg Linch <glinch@mcclatchydc.com>
2017-10-15 20:03:00 -0400

    update url patterns to use new class-based views and move source app-specific urls to that module

--- sourcelist/urls.py
+++ sourcelist/urls.py
@@ -2,25 +2,14 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from sources.views import about, confirm, contact, index, join, results, thankyou
 
 
 urlpatterns = [
     ## patterns from sources app
-	url(r'^sources/', include('sources.urls')),
+	# url(r'^sources/', include('sources.urls')),
+    url(r'^', include('sources.urls')),
     ## general
     url(r'^admin/', admin.site.urls),
-    ## site pages
-    url(r'^$', index, name='index'),
-    url(r'^index$', index, name='index'),
-    url(r'^about$', about, name='about'),
-    url(r'^contact$', contact, name='contact'),
-    url(r'^results$', results, name='results'),
-    ## submission form
-    url(r'^confirmation/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        confirm, name='confirm'), ## UPDATE?!?!
-    url(r'^join', join, name='join'),
-    url(r'^thank-you', thankyou, name='thank-you'),
     ## social auth
     url(r'^accounts/login/$', auth_views.LoginView.as_view()),
     # url('', include('social.apps.django_app.urls', namespace='social')),
--- sources/urls.py
+++ sources/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
-from . import views
+from sources.views import AboutView, ConfirmView, ContactView, IndexView, JoinView, ResultsView, ThankYouView
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+	## main pages
+    url(r'^$', IndexView.as_view()),
+    url(r'^index$', IndexView.as_view()),
+    url(r'^about$', AboutView.as_view()),
+    url(r'^contact$', ContactView.as_view()),
+    url(r'^results$', ResultsView.as_view()),
+    ## submission form
+    url(r'^confirmation/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        ConfirmView.as_view()),
+    url(r'^join', JoinView.as_view()),
+    url(r'^thank-you', ThankYouView.as_view()),
 ]
