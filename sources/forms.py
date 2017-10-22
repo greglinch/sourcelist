072ec4b79bf648704e77e9c0d7d39a938e7dfc9b
Greg Linch <glinch@mcclatchydc.com>
2017-10-22 10:57:42 -0400

    remove status from form

--- sources/forms.py
+++ sources/forms.py
@@ -13,4 +13,4 @@ class ContactForm(forms.Form):
 class SubmitForm(forms.ModelForm):
     class Meta:
         model = Person
-        fields = ['prefix', 'first_name', 'middle_name', 'last_name', 'type_of_scientist', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'notes', 'language', 'timezone', 'city', 'state', 'country', 'status']
+        fields = ['prefix', 'first_name', 'middle_name', 'last_name', 'type_of_scientist', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'notes', 'language', 'timezone', 'city', 'state', 'country']
