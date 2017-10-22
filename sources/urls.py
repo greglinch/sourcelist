d7599883f03a00d85f883bd83a990757babdf47e
Greg Linch <glinch@mcclatchydc.com>
2017-10-22 10:10:48 -0400

    add person detail view to URLs

--- sources/urls.py
+++ sources/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from sources.views import AboutView, ConfirmView, ContactView, IndexView, JoinView, ResultsView, ThankYouView
+from sources.views import AboutView, ConfirmView, ContactView, DetailView, IndexView, JoinView, ResultsView, ThankYouView
 
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^about$', AboutView.as_view()),
     url(r'^contact$', ContactView.as_view()),
     url(r'^results$', ResultsView.as_view()),
+    url(r'^(?P<slug>[-\w]+)/$', DetailView.as_view(), name='source'),
     ## submission form
     url(r'^confirmation/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         ConfirmView.as_view()),
