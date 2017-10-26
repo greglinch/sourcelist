9caccb81fe0b31c0857ed25d37eb52a5f7ce79ae
Greg Linch <glinch@mcclatchydc.com>
2017-10-26 12:06:53 -0400

    updated import csv script with detailed output of errors and skipping import of those

--- sources/management/commands/import_csv.py
+++ sources/management/commands/import_csv.py
@@ -17,22 +17,22 @@ def import_csv(csv_path):
     print(message)
 
     ## read csv
-    # csv_path = '/Users/glinch/Downloads/sources.csv'
     with open(csv_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
-        row_count = sum(1 for row in csv_reader)
-        message = 'Number of rows: {}\t'.format(row_count)
-        print(message)
+        # row_count = sum(1 for row in csv_reader)
+        # message = 'Number of rows: {}\t'.format(row_count)
+        # print(message)
         counter = 0
-        failed_rows = []
+        failed_rows = 0
+        # import pdb; pdb.set_trace()
         ## loops thru the rows
         for row in csv_reader:
             counter += 1
             ## special fields
             status = 'added_by_admin'
-            email_address = row['email_primary']
-            if isinstance(row['time_zone'], int):
-                timezone = row['time_zone']
+            email_address = row['email_address']
+            if isinstance(row['timezone'], int):
+                timezone = row['timezone']
             else:
                 timezone = None
             ## map fields from csv to Person model
@@ -40,7 +40,7 @@ def import_csv(csv_path):
                 'role': row['role'],
                 'first_name': row['first_name'],
                 'last_name': row['last_name'],
-                'type_of_expert': row['type'],
+                'type_of_expert': row['type_of_expert'],
                 'expertise': row['expertise'], ## m2m field
                 'title': row['title'],
                 'organization': row['organization'], ## m2m field
@@ -51,17 +51,27 @@ def import_csv(csv_path):
                 'phone_number_secondary': row['phone_secondary'],
                 'twitter': row['twitter'],
                 'notes': row['notes'],
-                'website': row['website'],
+                # 'website': row['website'],
                 'prefix': row['prefix'], 
                 # 'middle_name': '',
                 # 'language': 'English', ## m2mfield
+                'entry_type': 'automated',
                 'email_address': email_address,
                 'status': status,
                 'timezone': timezone,
             }
             ## create the source Person
             # try:
-            obj, created = Person.objects.update_or_create(**csv_to_model)
+            try:
+                exists = Person.objects.get(email_address=email_address)
+                counter -= 1
+            except:
+                try:
+                    obj, created = Person.objects.update_or_create(**csv_to_model)
+                except Exception as e:
+                    failed_rows += 1
+                    message = 'Row {}: {}\n'.format(counter, e)
+                    print(message)
             # except:
                 # failed_rows.append(counter)
             # try:
@@ -70,19 +80,19 @@ def import_csv(csv_path):
             #     message = 'Create person' + str(sys.exc_info())
             #     print(message)
             ## set the related user and email them
-            try:
-                call_command('set_related_user', email_address)
-            except:
-                message = 'Set related user: ' + str(sys.exc_info())
-                print(message)
+            # try:
+            #     call_command('set_related_user', email_address)
+            # except:
+            #     message = 'Set related user: ' + str(sys.exc_info())
+            #     print(message)
             # if not TEST_ENV:
                 # try:
                 #     call_command('email_user', email_address, status)
                 # except:
                 #     message = 'Email user:' + str(sys.exc_info())
                 #     print(message)
-            message = '\nThe following rows failed: \n\n {}'.format(failed_rows)
-            print(message)
+    # message = '\nThe following rows failed: \n\n {}'.format(failed_rows)
+    # print(message)
 
     ## end
     end_time = datetime.now()
@@ -94,6 +104,8 @@ def import_csv(csv_path):
     print(message)
 
     message = 'Imported {} rows'.format(counter)
+    print(message)
+    message = '{} rows failed'.format(failed_rows)
     print(message)
 
 class Command(BaseCommand):
