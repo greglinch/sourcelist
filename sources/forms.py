287aac165698b852cf28a9fdd83dab7487e07b13
Greg Linch <glinch@mcclatchydc.com>
2017-11-01 17:30:20 -0400

    comment out media field for now

--- sources/forms.py
+++ sources/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from sources.models import Person
+# from sources.choices import MEDIA_CHOICES
 
+
+FIELDS_PUBLIC = ['prefix', 'pronouns', 'first_name', 'middle_name', 'last_name', 'type_of_expert', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'twitter', 'language', 'timezone', 'city', 'state', 'country', 'notes']#, 'media']
 
 class ContactForm(forms.Form):
     name = forms.CharField(required=True)
@@ -11,6 +14,12 @@ class ContactForm(forms.Form):
     )
 
 class SubmitForm(forms.ModelForm):
+    # media_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=MEDIA_CHOICES) #, verbose_name='Which types of media are you interested interested or experienced in being a source?', help_text='Choose all that apply.')
+
     class Meta:
         model = Person
-        fields = ['prefix', 'pronouns', 'first_name', 'middle_name', 'last_name', 'type_of_expert', 'title', 'organization', 'website', 'expertise', 'email_address', 'phone_number_primary', 'phone_number_secondary', 'twitter', 'language', 'timezone', 'city', 'state', 'country', 'notes']
+        # fields_to_use = FIELDS_PUBLIC
+        # fields_to_use.pop(-1)
+        # fields_to_use.append('media_field')
+        fields = FIELDS_PUBLIC
+        
