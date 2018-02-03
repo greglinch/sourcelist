6bf76c99941c98659f72482e638d97f9bd1d67c7
Greg Linch <greglinch@gmail.com>
2018-02-02 22:40:05 -0500

    update timezone_format filter to add try/except and convert value to int before comparing

--- sources/templatetags/sources_extras.py
+++ sources/templatetags/sources_extras.py
@@ -6,6 +6,9 @@ register = template.Library()
 @register.filter
 def timezone_format(value):
 	""" Check the value of the timezeone offset and, if postive, add a plus sign"""
-	if value > 0:
-		value = '+' + str(value)
+	try:
+		if int(value) > 0:
+			value = '+' + str(value)
+	except:
+		pass
 	return value
