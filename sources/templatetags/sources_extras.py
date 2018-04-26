ac5fb9d99db99076b5a3dda2e2f33fb30bc5d3da
Greg Linch <greglinch@gmail.com>
2018-04-25 23:11:36 -0400

    add custom filter to reformat special characters as HTML for some level of spam-avoidance and add to template

--- sources/templatetags/sources_extras.py
+++ sources/templatetags/sources_extras.py
@@ -5,10 +5,28 @@ register = template.Library()
 
 @register.filter
 def timezone_format(value):
-	""" Check the value of the timezeone offset and, if postive, add a plus sign"""
-	try:
-		if int(value) > 0:
-			value = '+' + str(value)
-	except:
-		value = ''
-	return value
+    """ Check the value of the timezeone offset and, if postive, add a plus sign"""
+    try:
+        if int(value) > 0:
+            value = '+' + str(value)
+    except:
+        value = ''
+    return value
+
+
+@register.filter
+def reformat_email(value):
+    """ encode special characters HTML encoding to make it a little harder to scrape """
+    try:
+        value = value.replace('@', '&#64;')
+    except:
+        pass
+    try:
+        value = value.replace('.', '&#46;')
+    except:
+        pass
+    try:
+        value = value.replace('_', '&#95;')
+    except:
+        pass
+    return value
