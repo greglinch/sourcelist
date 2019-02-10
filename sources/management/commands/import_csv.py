f8433cee9124888572e80508f08bb44091459671
Greg Linch <greglinch@gmail.com>
2019-02-10 09:06:23 -0500

    WIP for updating import script to handle exported csv

--- sources/management/commands/import_csv.py
+++ sources/management/commands/import_csv.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 from django.contrib.auth.models import User, Group
+from django.utils import timezone
 from sources.models import Person
 from sourcelist.settings import TEST_ENV
 from datetime import datetime
@@ -8,94 +9,111 @@ import sys
 import csv
 
 
+## create the source Person
+def create_person(counter, failed_rows, data_dict):
+    # check if the person already exists:
+    try:
+        exists = Person.objects.get(email_address=email_address)
+        counter -= 1
+    except:
+        try:
+            obj, created = Person.objects.update_or_create(**data_dict)
+        except Exception as e:
+            failed_rows += 1
+            message = f'Row {counter}: {e}\n'
+            print(message)
+    # except:
+        # failed_rows.append(counter)
+    # try:
+    #     obj, created = Person.objects.create(**csv_to_model)
+    # except:
+    #     message = 'Create person' + str(sys.exc_info())
+    #     print(message)
+    ## set the related user and email them
+    try:
+        call_command('set_related_user', email_address)
+    except:
+        message = 'Set related user: ' + str(sys.exc_info())
+    #     print(message)
+    # if not TEST_ENV:
+        # try:
+        #     call_command('email_user', email_address, status)
+        # except:
+        #     message = 'Email user:' + str(sys.exc_info())
+        #     print(message)
+
 # def import_csv():
-def import_csv(csv_path):    
+def import_csv(csv_file):
     ## start
     start_time = datetime.now()
     start_message = '\nStarted import:\t {}\n'.format(start_time)
     message = start_message
     print(message)
 
-    ## read csv
-    with open(csv_path) as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        # row_count = sum(1 for row in csv_reader)
-        # message = 'Number of rows: {}\t'.format(row_count)
-        # print(message)
-        counter = 0
-        failed_rows = 0
-        # import pdb; pdb.set_trace()
-        ## loops thru the rows
-        for row in csv_reader:
-            counter += 1
-            ## special fields
-            status = 'added_by_admin'
-            email_address = row['email_address']
-            if isinstance(row['timezone'], int):
-                timezone = row['timezone']
-            else:
-                timezone = None
-            ## map fields from csv to Person model
-            csv_to_model = {
-                'role': row['role'],
-                'first_name': row['first_name'],
-                'last_name': row['last_name'],
-                'type_of_expert': row['type_of_expert'],
-                'expertise': row['expertise'], ## m2m field
-                'title': row['title'],
-                'organization': row['organization'], ## m2m field
-                'city': row['city'],
-                'state': row['state'],
-                'country': row['country'],
-                'phone_number_primary': row['phone_primary'],
-                'phone_number_secondary': row['phone_secondary'],
-                'twitter': row['twitter'],
-                'notes': row['notes'],
-                # 'website': row['website'],
-                'prefix': row['prefix'], 
-                # 'middle_name': '',
-                # 'language': 'English', ## m2mfield
-                'approved_by_admin': True,
-                'approved_by_user': True,
-                'entry_method': 'import',
-                'entry_type': 'automated',
-                'email_address': email_address,
-                'status': status,
-                'timezone': timezone,
-            }
-            ## create the source Person
-            # try:
-            try:
-                exists = Person.objects.get(email_address=email_address)
-                counter -= 1
-            except:
-                try:
-                    obj, created = Person.objects.update_or_create(**csv_to_model)
-                except Exception as e:
-                    failed_rows += 1
-                    message = 'Row {}: {}\n'.format(counter, e)
-                    print(message)
-            # except:
-                # failed_rows.append(counter)
-            # try:
-            #     obj, created = Person.objects.create(**csv_to_model)
-            # except:
-            #     message = 'Create person' + str(sys.exc_info())
-            #     print(message)
-            ## set the related user and email them
-            try:
-                call_command('set_related_user', email_address)
-            except:
-                message = 'Set related user: ' + str(sys.exc_info())
-            #     print(message)
-            # if not TEST_ENV:
-                # try:
-                #     call_command('email_user', email_address, status)
-                # except:
-                #     message = 'Email user:' + str(sys.exc_info())
-                #     print(message)
-    # message = '\nThe following rows failed: \n\n {}'.format(failed_rows)
+    # row_count = sum(1 for row in csv_reader)
+    # message = 'Number of rows: {}\t'.format(row_count)
     # print(message)
+    counter = 0
+    failed_rows = 0
+
+    if 'latest_export.csv' in csv_file:
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                row_as_dict = dict(row)
+                now = str(timezone.now())
+                if row_as_dict['created'] == '':
+                    row_as_dict['created'] = now
+                if row_as_dict['updated'] == '':
+                    row_as_dict['updated'] = now
+                if row_as_dict['timezone'] == '':
+                    row_as_dict.pop('timezone')
+                # import pdb; pdb.set_trace()
+                create_person(counter, failed_rows, row_as_dict)
+    else:
+        with open(csv_file) as file:
+            csv_reader = csv.DictReader(file)
+            ## loops thru the rows
+            for row in csv_reader:
+                counter += 1
+                ## special fields
+                status = 'added_by_admin'
+                email_address = row['email_address']
+                if isinstance(row['timezone'], int):
+                    timezone_value = row['timezone']
+                else:
+                    timezone_value = None
+                ## map fields from csv to Person model
+                csv_to_model_dict = {
+                    'role': row['role'],
+                    'first_name': row['first_name'],
+                    'last_name': row['last_name'],
+                    'type_of_expert': row['type_of_expert'],
+                    'expertise': row['expertise'], ## m2m field
+                    'title': row['title'],
+                    'organization': row['organization'], ## m2m field
+                    'city': row['city'],
+                    'state': row['state'],
+                    'country': row['country'],
+                    'phone_number_primary': row['phone_primary'],
+                    'phone_number_secondary': row['phone_secondary'],
+                    'twitter': row['twitter'],
+                    'notes': row['notes'],
+                    # 'website': row['website'],
+                    'prefix': row['prefix'],
+                    # 'middle_name': '',
+                    # 'language': 'English', ## m2mfield
+                    'approved_by_admin': True,
+                    'approved_by_user': True,
+                    'entry_method': 'import',
+                    'entry_type': 'automated',
+                    'email_address': email_address,
+                    'status': status,
+                    'timezone': timezone_value,
+                }
+                create_person(csv_to_model_dict)
+        # message = '\nThe following rows failed: \n\n {}'.format(failed_rows)
+        # print(message)
 
     ## end
     end_time = datetime.now()
@@ -131,9 +149,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         ## unpack args
-        csv_path = options['file']
+        csv_file = options['file']
 
         ## call the function
         # import_csv()
-        import_csv(csv_path)
+        import_csv(csv_file)
 
