6eb2e3752ef46cd55944fb282e45afe925f73310
Greg Linch <greglinch@gmail.com>
2019-02-16 18:23:09 -0500

    remove unused import statement

--- sources/management/commands/set_related_user.py
+++ sources/management/commands/set_related_user.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.core.mail import send_mail
 from django.contrib.auth.models import User, Group
 from django.contrib import messages
 from sources.models import Person
