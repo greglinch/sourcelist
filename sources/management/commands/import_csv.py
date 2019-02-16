6bc869d215386b31dad83b12a578baf12d5338e7
Greg Linch <greglinch@gmail.com>
2019-02-16 18:15:10 -0500

    fix import statement grouping/ordering

--- sources/management/commands/import_csv.py
+++ sources/management/commands/import_csv.py
@@ -1,12 +1,14 @@
+import csv
+from datetime import datetime
+import sys
+
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import call_command
 from django.contrib.auth.models import User, Group
 from django.utils import timezone
-from sources.models import Person
+
 from sourcelist.settings import TEST_ENV
-from datetime import datetime
-import sys
-import csv
+from sources.models import Person
 
 
 ## create the source Person
