884d035d26071c5d6646022d219e8cf6cb23b67b
Greg Linch <greglinch@gmail.com>
2020-06-02 22:10:27 -0400

    realize the mgmt cmd code was a huge was of time and make it just call the save method for each source to do the same thing

--- sources/management/commands/generate_contact_images.py
+++ sources/management/commands/generate_contact_images.py
@@ -8,46 +8,7 @@ def generate_images():
     sources = Person.objects.all()
 
     for source in sources:
-        source_id = source.id
-
-        # email address
-        # try:
-        image_location = generate_image_from_text(
-            source_id,
-            source.email_address,
-            'email_address'
-        )
-        source.email_address_image = image_location
         source.save()
-        # except:
-        #     print(f'Failed: email_address - {source_id}')
-
-        # phone number primary
-        # try:
-        image_location = generate_image_from_text(
-            source_id,
-            source.phone_number_primary,
-            'phone_number_primary'
-        )
-        source.phone_number_primary_image = image_location
-        source.save()
-        # except:
-            # print(f'Failed: phone_number_primary - {source_id}')
-
-        # phone number secondary
-        # try:
-        if source.phone_number_secondary:
-            image_location = generate_image_from_text(
-                source.id,
-                source.phone_number_secondary,
-                'phone_number_secondary'
-            )
-            source.phone_number_secondary_image = image_location
-            source.save()
-        else:
-            print(f'N/A: phone_number_secondary - {source_id}')
-        # except:
-        #     print(f'Failed: phone_number_secondary - {source_id}')
 
 
 class Command(BaseCommand):
